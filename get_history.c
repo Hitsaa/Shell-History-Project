f5f092642625206455a5c558aff644d75a2a1151
Hitendra Singh Shekhawat <33285810+Hitsaa@users.noreply.github.com>
2019-07-25 21:36:30 +0530

    Update get_history.c

--- get_history.c
+++ get_history.c
@@ -1,16 +1,6 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
-
-#define MAX_LINE 100 
-#define BUFFER_SIZE 80	
-
-#define buffer "\n\Shell History:\n"
-
-char history[50][BUFFER_SIZE]; 
-int count = 0;
 
 //function to display the history of commands
 void displayHistory()
