389c24de1dd8abf3056dca980b59b3c2a8fc75ce
Hitendra Singh Shekhawat <33285810+Hitsaa@users.noreply.github.com>
2019-07-25 21:43:40 +0530

    Update macros.h

--- macros.h
+++ macros.h
@@ -1,3 +1,7 @@
+#ifndef MACROS_H
+#define MACROS_H
+
+#include <stddef.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -8,3 +12,13 @@
 #define BUFFER_SIZE 80	
 
 #define buffer "\n\Shell History:\n"
+char history[50][BUFFER_SIZE]; 
+int count = 0;
+
+//function to display the history of commands
+void displayHistory()
+
+//Fuction to get the command from shell, tokenize it and set the args parameter
+int formatCommand(char inputBuffer[], char *args[],int *flag)
+
+#endif
