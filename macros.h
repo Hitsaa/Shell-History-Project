7a25fcf251bf56212435af407104c0a4f05a527b
Hitendra Singh Shekhawat <33285810+Hitsaa@users.noreply.github.com>
2019-07-25 21:35:01 +0530

    Create macros.h

---
+++ macros.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+
+#define MAX_LINE 100 
+#define BUFFER_SIZE 80	
+
+#define buffer "\n\Shell History:\n"
