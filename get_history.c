dcb3692008d5926c3766418f8c13d7887657d1d1
Hitendra Singh Shekhawat <33285810+Hitsaa@users.noreply.github.com>
2019-07-25 21:37:30 +0530

    Update get_history.c

--- get_history.c
+++ get_history.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <macros.h>
 #include <string.h>
 
 //function to display the history of commands
@@ -145,55 +146,4 @@ if(strcmp(args[0],"history")==0)	//when we type on the command line, "history"(n
 	if(count>10)
 	{ count=10;
 	}
-}
-
-int main(void)
-{
-    char inputBuffer[MAX_LINE]; /* buffer to hold the input command */
-    int flag; // equals 1 if a command is followed by "&"
-    char *args[MAX_LINE/2 + 1];/* max arguments */
-    int should_run =1;
-    
-    pid_t pid,tpid;
-    int i;
-   
-    
-    while (should_run) //infinite loop for shell prompt
-    {            
-        flag = 0; //flag =0 by default
-        printf("New_Shell>>$");
-        fflush(stdout);
-        if(-1!=formatCommand(inputBuffer,args,&flag)) // get next command  
-	{
-		pid = fork();
-        
-        	if (pid < 0)//if pid is less than 0, forking fails
-        	{
-            
-            		printf("Fork failed.\n");
-            		exit (1);
-        	}
-        
-       		 else if (pid == 0)//if pid ==0
-        	{
-            
-           	 	//command not executed
-            		if (execvp(args[0], args) == -1)
-           	 	{	
-		
-                		printf("Error executing command\n");
-            		}
-       		 }
-           
-        	else
-        	{
-            		i++;
-           	 	if (flag == 0)
-           		 {
-                		i++;
-      //          		wait(NULL);
-           		 }
-        	}
-   	 }
-     }
 }
